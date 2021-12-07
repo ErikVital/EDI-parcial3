6227561f565f33e7b6bad24e701b7a3a2ae2cf1d
ErikVital <89081103+ErikVital@users.noreply.github.com>
2021-12-07 09:03:51 -0600

    Update examen.c

--- examen.c
+++ examen.c
@@ -1,23 +1,18 @@
 /*
     Escriba un programa en C que dada la siguiente declaración de estructuras:
-
     typedef struct {
         char nombre[40];
         char pais[25];
     }DatosPersonales;
-
     typedef struct{
         DatosPersonales datos;
         char deporte[30];
         int numMedallas;
     }Deportista;
-
     1. Realice una función que lea de un archivo de TEXTO la información de los deportistas.
     2. Realice una función que reciba los deportistas leídos y calcule e imprima el número de medallas por
     deporte.
-
     Nota: Dentro del código existe un arreglo que contiene los nombres de todos los deportes que existen en el archivo.
-
         char deporte[6][30] = {"Natación", "Atletismo", "Ciclismo", "Gimnasia", "Equitacion", "Esgrima"};
 */
 
@@ -44,6 +39,89 @@ int main()
     // Puede cambiar la declaración de este arreglo a donde lo considere necesario
     char deporte[6][30] = {"Natación", "Atletismo", "Ciclismo", "Gimnasia", "Equitacion", "Esgrima"};
 
+	archivoLee(deportistas);
+
     return 0;
 }
 
+void archivoLee(struct Deportista dep[100])
+{
+	FILE *archivo;
+	int cerrado;
+	int i=0, j, k;
+
+	char read[50];
+	archivo = fopen("deportistas.txt", "r");//read
+
+	if(archivo==NULL)
+	{
+		printf("No se ha podido abrir");
+	}
+	
+	
+	while(!feof(archivo))
+	{
+		fgets(read, 50, archivo);
+		i++;
+	}
+	
+	rewind(archivo);
+	
+	dep = (Deportista*)malloc(i*sizeof(Deportista));
+	if(dep==NULL)
+	{
+		printf("No se ha podido");
+	}
+
+	for(j=0; !feof(archivo); j++)
+	{
+		vaciar(read);
+		aux = '0';
+		for(k=0; aux!='-'; k++)
+		{
+			aux=fgetc(archivo);
+			if(aux!='-')
+			{
+				read[k]=aux;
+			}
+		}
+		copiar(read, i);
+	}
+	
+	cerrado = fclose(archivo);
+	
+	if(cerrado == E0F)
+		printf("El archivo no se cerro bien");
+}
+
+void archivoRecibe()
+{
+	FILE *archivo;
+	int cerrado;
+	int i;
+	
+
+	archivo = fopen("deportistas.txt", "w");//write
+
+	
+	
+	
+	
+	do{
+		fscanf(archivo, "%s", nombre);
+		fscanf(archivo, "%d", &valor);
+		
+		printf("%s - %d", nombre, valor);
+	}while(finalArchivo != E0F);
+	
+
+	
+	
+	cerrado = fclose(archivo);
+	
+	if(cerrado == E0F)
+		primtf("El archivo no se cerro bien");
+		
+	return 0;
+}
+//me hice bolas
